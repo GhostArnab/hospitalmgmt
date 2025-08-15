e154ff2fdbcb1ab5a697833998ed6714c93629b7
GhostArnab <arnabdalui4343@gmail.com>
2025-08-15 10:43:46 +0530

    git init

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        String url= "jdbc:mysql://localhost:3306/hospital";
+        String username = "root";
+        String password = "Wind0ws@4343";
+
+        try (Connection connection= DriverManager.getConnection(url, username, password))
+        {
+            System.out.println("Connected");
+            System.out.println(connection);
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Failed: " + e.getMessage());
+        }
+    }
+}
